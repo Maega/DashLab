df425c06ac76c5e60f64772a25220e7a4809af1e
Tristan Gauci <tristan@maega.com.au>
2022-03-23 22:12:42 +1000

    Backup driver now executes tasks non-interactively

--- drivers/backup/backup.driver.js
+++ drivers/backup/backup.driver.js
@@ -3,9 +3,9 @@ const {shellExec} = require('../../common');
 const defaultPath = '/mnt/dashlab-backup';
 
 exports.backup = async function(path = defaultPath) {
-    await shellExec('sudo', ['/boot/dietpi/dietpi-backup', '1', path]);
+    await shellExec('sudo', ['G_INTERACTIVE=0', '/boot/dietpi/dietpi-backup', '1', path]);
 }
 
 exports.restore = async function(path = defaultPath) {
-    await shellExec('sudo', ['/boot/dietpi/dietpi-backup', '-1', path]);
+    await shellExec('sudo', ['G_INTERACTIVE=0', '/boot/dietpi/dietpi-backup', '-1', path]);
 }
