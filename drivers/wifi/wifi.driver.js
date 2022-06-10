c830d0fde7bddf479e499f768de9c68ccb9c5e4c
Tristan Gauci <tristan@maega.com.au>
2022-06-10 17:15:47 +1000

    Added change hostname to wifi driver

--- drivers/wifi/wifi.driver.js
+++ drivers/wifi/wifi.driver.js
@@ -1,4 +1,9 @@
 const Wifi = require('rpi-wifi-connection');
 const wifi = new Wifi();
+const {shellExec} = require('../../common');
+
+async function changeHostname(hostname) {
+    await shellExec('sudo', ['/boot/dietpi/func/change_hostname', hostname]);
+}
 
 module.exports = wifi;
